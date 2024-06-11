1446c63ff0306610cd4402d8e7354cf6799c257b
Kay Freyer <kay.freyer@icloud.com>
2024-06-11 15:17:33 +0200

    Make backup of engine with Parser

---
+++ compiler/engineParser/errors.go
@@ -0,0 +1,76 @@
+package engine
+
+import (
+	"fmt"
+	"hack/compiler/token"
+)
+
+type ErrSyntaxUnexpectedToken struct {
+	ExpectedToken string
+	FoundToken    string
+}
+
+func NewErrSyntaxUnexpectedToken(expectedToken, foundToken string) ErrSyntaxUnexpectedToken {
+	return ErrSyntaxUnexpectedToken{
+		ExpectedToken: expectedToken,
+		FoundToken:    foundToken,
+	}
+}
+
+func (err ErrSyntaxUnexpectedToken) Error() string {
+	return fmt.Sprintf("expected %s, found %q", err.ExpectedToken, err.FoundToken)
+}
+
+func NewErrSyntaxNotATerm(foundToken string) ErrSyntaxUnexpectedToken {
+	return NewErrSyntaxUnexpectedToken(
+		fmt.Sprintf(
+			"TOKENTYPE '%s' / '%s' / '%s' / '%s' or SYMBOL '%s' / '%s' / '%s'",
+			token.INT_CONST, token.STRING_CONST, token.KEYWORD, token.IDENTIFIER, token.LPAREN, token.TILDE, token.MINUS,
+		),
+		foundToken,
+	)
+}
+
+func NewErrSyntaxNotAKeywordConst(foundToken string) ErrSyntaxUnexpectedToken {
+	return NewErrSyntaxUnexpectedToken(
+		fmt.Sprintf("KEYWORD %s / %s / %s / %s", token.TRUE, token.FALSE, token.NULL, token.THIS),
+		foundToken,
+	)
+}
+
+func NewErrSyntaxNotAType(foundToken string) ErrSyntaxUnexpectedToken {
+	return NewErrSyntaxUnexpectedToken(
+		fmt.Sprintf("KEYWORD %s / %s / %s or className", token.INT, token.CHAR, token.BOOLEAN),
+		foundToken,
+	)
+}
+
+func NewErrSyntaxNotAClassVarDec(foundToken string) ErrSyntaxUnexpectedToken {
+	return NewErrSyntaxUnexpectedToken(
+		fmt.Sprintf("KEYWORD %s or %s ", token.STATIC, token.FIELD),
+		foundToken,
+	)
+}
+
+func NewErrSyntaxNotASubroutineDec(foundToken string) ErrSyntaxUnexpectedToken {
+	return NewErrSyntaxUnexpectedToken(
+		fmt.Sprintf("KEYWORD %s / %s / %s ", token.CONSTRUCTOR, token.FUNCTION, token.METHOD),
+		foundToken,
+	)
+}
+
+type ErrSyntaxUnexpectedTokenType struct {
+	ExpectedTokenType token.TokenType
+	FoundToken        string
+}
+
+func NewErrSyntaxUnexpectedTokenType(expectedTokenType token.TokenType, foundToken string) ErrSyntaxUnexpectedTokenType {
+	return ErrSyntaxUnexpectedTokenType{
+		ExpectedTokenType: expectedTokenType,
+		FoundToken:        foundToken,
+	}
+}
+
+func (err ErrSyntaxUnexpectedTokenType) Error() string {
+	return fmt.Sprintf("expected tokenType %s but found tokenType %s", err.ExpectedTokenType, err.FoundToken)
+}
